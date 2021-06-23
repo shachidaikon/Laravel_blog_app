c9bbdeed30eb8691e01f553fc160db44ed69f59a
shachidaikon <tkroxy0815@gmail.com>
2021-06-23 16:29:18 +0900

    [add]記事作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,4 +14,9 @@ class PostController extends Controller
     public function show(Post $post){
         return view('show')->with(['post' => $post]);
     }
+    
+    public function create()
+    {
+        return view('create');
+    }
 }
