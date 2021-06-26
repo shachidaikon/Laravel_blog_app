75cd035eaa57e4a7c5b7516f1078d77d3ba41805
shachidaikon <tkroxy0815@gmail.com>
2021-06-26 19:08:16 +0900

    [add]編集機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,10 +20,25 @@ class PostController extends Controller
         return view('create');
     }
     
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    
+    
     public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/'.$post->id);
     }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+
 }
